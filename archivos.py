26257153c5e0230724426accc0a64063d53cc811
Jorge Salleg <jorge.salleg@gimsaber.edu.co>
2024-10-08 16:53:40 -0500

    code

--- archivos.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  8 15:53:45 2024
-
-@author: jorge
-"""
-
--- principal.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct  8 09:39:28 2024
-
-@author: jorge
-"""
-print("hola mundo!")
-print("Hola todos")
